090a95fe5c15f7e9329bfcdafa185ecfddee84a6
Michael-Bryant Choa <michael-bryant.choa@hautelook.com>
2017-04-25 18:54:13 -0700

    Use heapdump for Chrome profile snapshots

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 var http = require('http');
+var memwatch = require('memwatch-next');
+var heapdump = require('heapdump');
+var partialRight = require('lodash.partialright');
+
+var LEAK_RATE = 500;
+
+var writeSnapshot = partialRight(heapdump.writeSnapshot, function (err, filename) {
+    if (err) console.error(err);
+    console.error('Heapdump file written to ', filename);
+});
+
+memwatch.on('leak', function (info) {
+    console.error('Memory leak detected: ', info);
+    writeSnapshot();
+});
+
+memwatch.on('stats', function (stats) {
+    console.log('Memory stats: ', stats);
+});
 
 var server = http.createServer(function (req, res) {
-    for (let i=0; i<1000; i++) {
+    for (let i=0; i<LEAK_RATE; i++) {
         server.on('request', function leakyFunc () {});
     }
 
     res.end('Hello World\n');
 }).listen(3417, 'localhost');
 server.setMaxListeners(0);
+
 console.log('Server running at http://localhost:3417. Process PID: ', process.pid);
